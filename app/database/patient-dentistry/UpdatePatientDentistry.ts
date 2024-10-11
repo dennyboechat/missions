f5f70f2ec93861d858c07893c935b20bba13ec52
Denny <dennyboechat@hotmail.com>
2024-10-11 20:59:56 +1300

    Retry attempt

--- app/database/patient-dentistry/UpdatePatientDentistry.ts
+++ app/database/patient-dentistry/UpdatePatientDentistry.ts
@@ -32,31 +32,34 @@ export const updatePatientDentistry = async ({
 
       const validatedValue = typeof value === "string" ? value.trim() : value;
 
-      const response = await sql.query(query, [
-        validatedValue,
-        patientDentistryId,
-      ]);
+      const response = await sql
+        .query(query, [validatedValue, patientDentistryId])
+        .then((response) => {
+          return response;
+        })
+        .catch(async (error) => {
+          attempt++;
+          console.error(`Attempt ${attempt} failed:`, error);
 
-      const patientDentistries: PatientDental[] = response.rows.map((row) => ({
-        patientDentistryId: row.patient_dentistry_id,
-        patientPersonalId: row.patient_personal_id,
-        appointmentNotes: row.appointment_notes,
-        appointmentDate: row.appointment_date,
-      }));
+          if (attempt >= DatabaseRetries) {
+            console.error("Max retries reached.");
+            return undefined;
+          }
+
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        });
+
+      const patientDentistries: PatientDental[] | undefined =
+        response?.rows.map((row) => ({
+          patientDentistryId: row.patient_dentistry_id,
+          patientPersonalId: row.patient_personal_id,
+          appointmentNotes: row.appointment_notes,
+          appointmentDate: row.appointment_date,
+        }));
 
       return patientDentistries && patientDentistries.length > 0
         ? patientDentistries[0]
         : undefined;
-    } catch (error) {
-      attempt++;
-      console.error(`Attempt ${attempt} failed:`, error);
-
-      if (attempt >= DatabaseRetries) {
-        console.error("Max retries reached.");
-        return undefined;
-      }
-
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
+    } catch (error) {}
   }
 };
